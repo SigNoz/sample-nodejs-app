4595ec09571bea2c9b63d6be1cc38ba09d3f0f4e
Ankit Nayan <ankit@signoz.io>
2022-07-07 10:25:38 +0530

    Revert "feat: add support for pm2 cluster mode"

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const { init } = require('./tracing')
-const api = require('@opentelemetry/api')
-init('demo-node-service', 'development')
+const express = require('express');
 
-const express = require('express')
-const axios = require('axios')
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.get('/hello', (req, res) => {
+ res.status(200).send('Hello World');
+});
 
-app.get('/hello', async (req, res) => {
-    const apiResponse = await axios.get('https://randomuser.me/api/')
-    // const activeSpan = api.trace.getSpan(api.context.active())
-    // activeSpan.addEvent('Hello API Called', { randomIndex: 1 })
-    res.status(200).send({
-        success: true,
-        result: apiResponse.data,
-    })
-})
-
-app.listen(3000, (req, res) => {
-    console.log('server started')
-})
+app.listen(9090);
