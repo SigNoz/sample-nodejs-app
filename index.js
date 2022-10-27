fe0d7e3d0820f3743631edc26e1b6db46bec6e59
makeavish <makeavish786@gmail.com>
2022-10-27 23:39:15 +0530

    move to http exporter and init tracing.js in index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const { init } = require('./tracing')
+const api = require('@opentelemetry/api')
+init('demo-node-service', 'development')
 
+const express = require("express");
+const cors = require('cors')
+const PORT = process.env.PORT || "5555";
 const app = express();
 
-app.get('/hello', (req, res) => {
- res.status(200).send('Hello World');
+app.use(cors());
+app.use(express.json())
+
+app.all("/", (req, res) => {
+    res.json({ method: req.method, message: "Hello World", ...req.body });
 });
 
-app.listen(9090);
+app.get('/404', (req, res) => {
+    res.sendStatus(404);
+})
+
+app.listen(parseInt(PORT, 10), () => {
+    console.log(`Listening for requests on http://localhost:${PORT}`);
+});
